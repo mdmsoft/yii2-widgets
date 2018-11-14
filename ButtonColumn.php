50fee5e7627ac9cda331a8bd1066faf43b99f71c
mdmunir <misbahuldmunir@gmail.com>
2018-11-14 12:17:27 +0700

    Option show button add

--- ButtonColumn.php
+++ ButtonColumn.php
@@ -13,6 +13,11 @@ use yii\helpers\Html;
 class ButtonColumn extends Column
 {
     /**
+     *
+     * @var boolean Show add button 
+     */
+    public $showButtonAdd = true;
+    /**
      * @var string Icon for header
      */
     public $headerIcon = '<span class="glyphicon glyphicon-plus"></span>';
@@ -26,10 +31,12 @@ class ButtonColumn extends Column
      */
     public function init()
     {
-        $id = $this->grid->options['id'] . '-add-button';
-        $this->header = Html::a($this->headerIcon, '#', ['id' => $id]);
-        if (!isset($this->grid->clientOptions['btnAddSelector'])) {
-            $this->grid->clientOptions['btnAddSelector'] = '#' . $id;
+        if ($this->showButtonAdd) {
+            $id = $this->grid->options['id'] . '-add-button';
+            $this->header = Html::a($this->headerIcon, '#', ['id' => $id]);
+            if (!isset($this->grid->clientOptions['btnAddSelector'])) {
+                $this->grid->clientOptions['btnAddSelector'] = '#' . $id;
+            }
         }
         $this->value = Html::a($this->deleteIcon, '#', ['data-action' => 'delete']);
     }
